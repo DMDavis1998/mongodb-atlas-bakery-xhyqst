3f8cd0aaa69bc1440b21adf069505ecc2ac43778
DMDavis1998 <34146233+DMDavis1998@users.noreply.github.com>
2022-11-16 08:22:53 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mongodb-atlas-bakery-xhyqst
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/mongodb-atlas-bakery-xhyqst)
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  APP_ID: 'bakery-zfdis',
+  GRAPHQL_URI:
+    'https://europe-west1.gcp.realm.mongodb.com/api/client/v2.0/app/bakery-zfdis/graphql',
+  API_KEY: '8kyQFgXNWVIwa77RHRxtVmkektGzxZH7ONt9zeoT47Sd5BSKAUEDlr6wTQ9FzaCH',
+};
